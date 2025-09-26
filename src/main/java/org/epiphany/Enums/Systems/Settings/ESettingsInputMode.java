4c2c62493bc84f7f3be92788b1a81425daef4aff
thomasHermJr <tjherm01@louisville.edu>
2025-09-26 07:41:16 -0400

    Replaced != with null-safe !equals

--- src/main/java/org/epiphany/Enums/Systems/Settings/ESettingsInputMode.java
+++ src/main/java/org/epiphany/Enums/Systems/Settings/ESettingsInputMode.java
@@ -1,5 +1,7 @@
 package org.epiphany.Enums.Systems.Settings;
 
+import java.util.Objects;
+
 public enum ESettingsInputMode { // start enum
 
     KEYBOARD_MOUSE("Keyboard & Mouse"),
@@ -8,8 +10,8 @@ public enum ESettingsInputMode { // start enum
     private final String inputName;
 
     ESettingsInputMode(String inputName) {
-        
-        if (inputName != "Keyboard & Mouse" && inputName != "Gamepad") { // validate input mode
+
+        if (!Objects.equals(inputName, "Keyboard & Mouse") && !Objects.equals(inputName, "Gamepad")) { // validate input mode
             inputName = "Keyboard & Mouse"; // default to Keyboard & Mouse if invalid
         } // end if
 
