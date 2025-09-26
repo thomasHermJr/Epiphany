0ccf1089f038e3733210c4bf02dcdc7be34f7225
thomasHermJr <tjherm01@louisville.edu>
2025-09-26 00:55:31 -0400

    Added stat inputs to battle overdrives

--- src/main/java/org/epiphany/Enums/Armors/EChainmails.java
+++ src/main/java/org/epiphany/Enums/Armors/EChainmails.java
@@ -1,4 +1,7 @@
 package org.epiphany.Enums.Armors;
 
 public enum EChainmails {
+
+
+
 }
--- src/main/java/org/epiphany/Enums/Systems/Battle/EBattleOverdrives.java
+++ src/main/java/org/epiphany/Enums/Systems/Battle/EBattleOverdrives.java
@@ -6,58 +6,63 @@ public enum EBattleOverdrives {
     /*Weapon Overdrives*/
 
     /*Level 1 OD*/
-    SPLICE("Splice"), // Sword OD
-    CRACK("Crack"), // Axe OD
-    IMPALE("Impale"), // Polearm OD
-    BULLSEYE("Bullseye"), // Bow OD
+    SPLICE("Splice", 1, 120), // Sword OD
+    CRACK("Crack", 1, 130), // Axe OD
+    IMPALE("Impale", 1, 160), // Polearm OD
+    BULLSEYE("Bullseye", 1, 140), // Bow OD
 
     /*Level 2 OD*/
-    SLICE_AND_DICE("Slice and Dice"), // Sword OD
-    HOOK_N_HACK("Hook-N-Hack"), // Axe OD
-    WHIRLWIND("Whirlwind"), // polearm OD
-    VOLLEY("Volley"), // Bow OD
+    SLICE_AND_DICE("Slice and Dice", 4, 115), // Sword OD
+    HOOK_N_HACK("Hook-N-Hack", 1, 150), // Axe OD
+    WHIRLWIND("Whirlwind", 4, 150), // polearm OD
+    VOLLEY("Volley", 4, 130), // Bow OD
 
     /*Level 3 OD*/
-    LAMB_CHOP("Lamb Chop"), // Axe OD
-    LIVER_PUDDING("Liver Pudding"), // Polearm OD
-    MINCE("Mince"), // Sword OD
-    FLAMBE("Flambe"), // Bow OD
+    LAMB_CHOP("Lamb Chop", 1, 180), // Axe OD
+    LIVER_PUDDING("Liver Pudding", 1, 220), // Polearm OD
+    MINCE("Mince", 1, 175), // Sword OD
+    FLAMBE("Flambe", 4, 160), // Bow OD
 
     /*Job Overdrives*/
 
     /*Acolyte job tree*/
 
     //insert acolyte job od here
-    ZEN_YANG("Zen Yang"), // Monk OD
-    KREUZ_AIDE("Kreuz Aide"), // Priest OD
+    ZEN_YANG("Zen Yang", 4, 100), // Monk OD - buffs party
+    KREUZ_AIDE("Kreuz Aide", 4, 200), // Priest OD - heals party
 
     /*Rogue job tree*/
 
     //insert rogue job od here
-    SHADOW_DANCE("Shadow Dance"), // Thief OD
-    PHOENIX_DANCE("Phoenix Dance"), // Dancer OD
+    SHADOW_DANCE("Shadow Dance", 1, 120), // Thief OD
+    PHOENIX_DANCE("Phoenix Dance", 4, 200), // Dancer OD
 
 
     /*Knight job tree*/
     //insert knight job od here
-    HOLY_RETRIBUTION("Holy Retribution"), // Crusader OD
-    DIVINE_JUDGEMENT("Divine Judgement"),// Paladin OD
+    HOLY_RETRIBUTION("Holy Retribution", 1, 150), // Crusader OD
+    DIVINE_JUDGEMENT("Divine Judgement", 1, 200),// Paladin OD
 
 
     /*Tactician job tree*/
 
-    REFORMATION("Reformation"), // Tactician OD
-    PINCER_ATTACK("Pincer Attack"), // Commander OD
-    REVITALIZE_AND_PRESERVE("Revitalize and Preserve"); // General OD
+    REFORMATION("Reformation", 4, 100), // Tactician OD
+    PINCER_ATTACK("Pincer Attack", 4, 140), // Commander OD
+    REVITALIZE_AND_PRESERVE("Revitalize and Preserve", 4, 200); // General OD
 
 
 
 
 
     private final String overdriveName;
+    private final int targetCount; // Default target count is 1, can be modified later for multi-target ODs
+    private final int powerPercentage; // Default power is 150, can be modified later for stronger/weaker ODs
 
-    EBattleOverdrives(String overdriveName) {
+
+    EBattleOverdrives(String overdriveName, int targetCount, int powerPercentage) {
         this.overdriveName = overdriveName;
+        this.targetCount = targetCount;
+        this.powerPercentage = powerPercentage;
     }
 
     public String getOverdriveName() {
