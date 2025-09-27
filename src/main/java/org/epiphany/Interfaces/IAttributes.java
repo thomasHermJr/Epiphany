18f954c56b9f8047df26eb81547fd0fae2230ef9
thomasHermJr <tjherm01@louisville.edu>
2025-09-27 00:09:36 -0400

    Working on updating RAttributes.java, will update IAttributes.java after

--- src/main/java/org/epiphany/Interfaces/IAttributes.java
+++ src/main/java/org/epiphany/Interfaces/IAttributes.java
@@ -21,12 +21,29 @@ public interface IAttributes extends ICharacter{ // open IAttributes interface
     byte physicalDefense = 0;
     byte speed = 0;
 
+    /*Overdrive Attributes*/
+
+    byte overdriveMeter = 0; // max 100, min 0
+    boolean overdriveActive = false;
+
     /*Weapon Proficiencies*/
 
     byte swordProficiency = 0; // balanced weapon (light and heavy)
     byte axeProficiency = 0; // heavy weapon
     byte bowProficiency = 0; // ranged weapon
     byte polearmProficiency = 0; // heavy weapon
+
+    short swordExperienceRequired = 100;
+    short axeExperienceRequired = 100;
+    short bowExperienceRequired = 100;
+    short polearmExperienceRequired = 100;
+
+    short swordGrowthRate = 120; // percentage change to needed experience per level
+    short axeGrowthRate = 120; // percentage change to needed experience per level
+    short bowGrowthRate = 120; // percentage change to needed experience per level
+    short polearmGrowthRate = 120; // percentage change to needed experience per level
+
+    byte weaponChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
 
     short swordExperience = 0;
     short axeExperience = 0;
@@ -39,6 +56,16 @@ public interface IAttributes extends ICharacter{ // open IAttributes interface
     byte chainmailProficiency = 0;
     byte plateProficiency = 0;
 
+    short gambesonExperienceRequired = 100;
+    short chainmailExperienceRequired = 100;
+    short plateExperienceRequired = 100;
+
+    short gambesonGrowthRate = 120; // percentage change to needed experience per level
+    short chainmailGrowthRate = 120; // percentage change to needed experience per level
+    short plateGrowthRate = 120; // percentage change to needed experience per level
+
+    byte armorChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
+
     short gambesonExperience = 0;
     short chainmailExperience = 0;
     short plateExperience = 0;
@@ -48,6 +75,16 @@ public interface IAttributes extends ICharacter{ // open IAttributes interface
     byte tacticalProficiency = 0;
     byte survivalProficiency = 0;
     byte medicalProficiency = 0;
+
+    short tacticalExperienceRequired = 100;
+    short survivalExperienceRequired = 100;
+    short medicalExperienceRequired = 100;
+
+    short tacticalGrowthRate = 120; // percentage change to needed experience per level
+    short survivalGrowthRate = 120; // percentage change to needed experience per level
+    short medicalGrowthRate = 120; // percentage change to needed experience per level
+
+    byte skillChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
 
     short tacticalExperience = 0;
     short survivalExperience = 0;
@@ -59,6 +96,16 @@ public interface IAttributes extends ICharacter{ // open IAttributes interface
     byte engineeringProficiency = 0;
     byte gadgetryProficiency = 0;
 
+    short smithingExperienceRequired = 100;
+    short engineeringExperienceRequired = 100;
+    short gadgetryExperienceRequired = 100;
+
+    short smithingGrowthRate = 120; // percentage change to needed experience per level
+    short engineeringGrowthRate = 120; // percentage change to needed experience per level
+    short gadgetryGrowthRate = 120; // percentage change to needed experience per level
+
+    byte lifeChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
+
     short smithingExperience = 0;
     short engineeringExperience = 0;
     short gadgetryExperience = 0;
@@ -68,6 +115,16 @@ public interface IAttributes extends ICharacter{ // open IAttributes interface
     byte leadershipProficiency = 0;
     byte barterProficiency = 0;
     byte oratoryProficiency = 0;
+
+    short leadershipExperienceRequired = 100;
+    short barterExperienceRequired = 100;
+    short oratoryExperienceRequired = 100;
+
+    short leadershipGrowthRate = 120; // percentage change to needed experience per level
+    short barterGrowthRate = 120; // percentage change to needed experience per level
+    short oratoryGrowthRate = 120; // percentage change to needed experience per level
+
+    byte personalChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
 
     short leadershipExperience = 0;
     short barterExperience = 0;
--- src/main/java/org/epiphany/Records/RAttributes.java
+++ src/main/java/org/epiphany/Records/RAttributes.java
@@ -21,11 +21,28 @@ public record RAttributes() implements IAttributes {
     static byte physicalDefense;
     static byte speed;
 
+    /* overdrive attributes */
+
+    static byte overdriveMeter; // max 100, min 0
+    static boolean overdriveActive;
+
     /* weapon proficiencies */
     static byte swordProficiency;
     static byte axeProficiency;
     static byte bowProficiency;
     static byte polearmProficiency;
+
+    static short swordExperienceRequired = 100;
+    static short axeExperienceRequired = 100;
+    static short bowExperienceRequired = 100;
+    static short polearmExperienceRequired = 100;
+
+    static short swordGrowthRate = 120; // percentage change to needed experience per level
+    static short axeGrowthRate = 120; // percentage change to needed experience per level
+    static short bowGrowthRate = 120; // percentage change to needed experience per level
+    static short polearmGrowthRate = 120; // percentage change to needed experience per
+
+    static byte weaponChangeRatePerLevel; // percentage change to growth rate per level, min 1 max 10
 
     static short swordExperience;
     static short axeExperience;
@@ -37,6 +54,16 @@ public record RAttributes() implements IAttributes {
     static byte chainmailProficiency;
     static byte plateProficiency;
 
+    static short gambesonExperienceRequired = 100;
+    static short chainmailExperienceRequired = 100;
+    static short plateExperienceRequired = 100;
+
+    static short gambesonGrowthRate = 120; // percentage change to needed experience per level
+    static short chainmailGrowthRate = 120; // percentage change to needed experience per level
+    static short plateGrowthRate = 120; // percentage change to needed experience per
+
+    static byte armorChangeRatePerLevel; // percentage change to growth rate per level, min 1 max 10
+
     static short gambesonExperience;
     static short chainmailExperience;
     static short plateExperience;
@@ -45,6 +72,21 @@ public record RAttributes() implements IAttributes {
     static byte tacticalProficiency;
     static byte survivalProficiency;
     static byte medicalProficiency;
+
+    static short tacticalExperienceRequired = 100;
+    static short survivalExperienceRequired = 100;
+    static short medicalExperienceRequired = 100;
+
+    static byte baseTacticalExperienceRequired = 100;
+    static byte baseSurvivalExperienceRequired = 100;
+    static byte baseMedicalExperienceRequired = 100;
+
+    static double skillChangeRatePerLevel = 1.05; // percentage change to growth rate per level, min 1 max 10
+    static double baseSkillGrowthRate = 1.20; // percentage change to needed experience per level
+
+    static double tacticalGrowthRate = baseSkillGrowthRate * skillChangeRatePerLevel * tacticalProficiency; // percentage change to needed experience per level
+    static double survivalGrowthRate = baseSkillGrowthRate * skillChangeRatePerLevel * survivalProficiency; // percentage change to needed experience per level
+    static double medicalGrowthRate = baseSkillGrowthRate * skillChangeRatePerLevel * medicalProficiency; // percentage change to needed experience per level
 
     static short tacticalExperience;
     static short survivalExperience;
@@ -55,6 +97,16 @@ public record RAttributes() implements IAttributes {
     static byte engineeringProficiency;
     static byte gadgetryProficiency;
 
+    static short smithingExperienceRequired = 100;
+    static short engineeringExperienceRequired = 100;
+    static short gadgetryExperienceRequired = 100;
+
+    static short smithingGrowthRate = 120; // percentage change to needed experience per level
+    static short engineeringGrowthRate = 120; // percentage change to needed experience per level
+    static short gadgetryGrowthRate = 120; // percentage change to needed experience per level
+
+    static byte lifeChangeRatePerLevel; // percentage change to growth rate per level, min 1 max 10
+
     static short smithingExperience;
     static short engineeringExperience;
     static short gadgetryExperience;
@@ -63,6 +115,16 @@ public record RAttributes() implements IAttributes {
     static byte leadershipProficiency;
     static byte barterProficiency;
     static byte oratoryProficiency;
+
+    static short leadershipExperienceRequired = 100;
+    static short barterExperienceRequired = 100;
+    static short oratoryExperienceRequired = 100;
+
+    static short leadershipGrowthRate = 120; // percentage change to needed experience per level
+    static short barterGrowthRate = 120; // percentage change to needed experience per level
+    static short oratoryGrowthRate = 120; // percentage change to needed experience per level
+
+    static byte personalChangeRatePerLevel; // percentage change to growth rate per level, min 1 max 10
 
     static short leadershipExperience;
     static short barterExperience;
@@ -177,12 +239,52 @@ public record RAttributes() implements IAttributes {
 
     } // end derived stat builder subclass
 
+    public static class OverdriveStatBuilder { // start overdrive stat builder
+
+        private byte overdriveMeter = 0; // max 100, min 0
+        private boolean overdriveActive = false;
+
+        public OverdriveStatBuilder setOverdriveMeter(byte overdriveMeter) {
+            if (overdriveMeter < 0) {
+                overdriveMeter = 0;
+            } else if (overdriveMeter > 100) {
+                overdriveMeter = 100;
+            }
+            this.overdriveMeter = overdriveMeter;
+            return this;
+        }
+
+        public OverdriveStatBuilder setOverdriveActive(boolean overdriveActive) {
+            this.overdriveActive = overdriveActive;
+            return this;
+        }
+
+        public RAttributes buildOverdrive() {
+            RAttributes.overdriveMeter = this.overdriveMeter;
+            RAttributes.overdriveActive = this.overdriveActive;
+            return new RAttributes();
+        }
+
+    } // end overdrive stat builder subclass
+
     public static class WeaponProficiencyBuilder { // start weapon proficiency builder
 
         private byte swordProficiency = 0;
         private byte axeProficiency = 0;
         private byte bowProficiency = 0;
         private byte polearmProficiency = 0;
+
+        private short swordExperienceRequired = 100;
+        private short axeExperienceRequired = 100;
+        private short bowExperienceRequired = 100;
+        private short polearmExperienceRequired = 100;
+
+        private short swordGrowthRate = 120; // percentage change to needed experience per level
+        private short axeGrowthRate = 120; // percentage change to needed experience per level
+        private short bowGrowthRate = 120; // percentage change to needed experience per level
+        private short polearmGrowthRate = 120; // percentage change to needed experience per level
+
+        private byte weaponChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
 
         private short swordExperience = 0;
         private short axeExperience = 0;
@@ -229,6 +331,51 @@ public record RAttributes() implements IAttributes {
             return this;
         }
 
+        public WeaponProficiencyBuilder setSwordExperienceRequired(short swordExperienceRequired) {
+            this.swordExperienceRequired = swordExperienceRequired;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setAxeExperienceRequired(short axeExperienceRequired) {
+            this.axeExperienceRequired = axeExperienceRequired;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setBowExperienceRequired(short bowExperienceRequired) {
+            this.bowExperienceRequired = bowExperienceRequired;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setPolearmExperienceRequired(short polearmExperienceRequired) {
+            this.polearmExperienceRequired = polearmExperienceRequired;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setSwordGrowthRate(short swordGrowthRate) {
+            this.swordGrowthRate = swordGrowthRate;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setAxeGrowthRate(short axeGrowthRate) {
+            this.axeGrowthRate = axeGrowthRate;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setBowGrowthRate(short bowGrowthRate) {
+            this.bowGrowthRate = bowGrowthRate;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setPolearmGrowthRate(short polearmGrowthRate) {
+            this.polearmGrowthRate = polearmGrowthRate;
+            return this;
+        }
+
+        public WeaponProficiencyBuilder setWeaponChangeRatePerLevel(byte weaponChangeRatePerLevel) {
+            this.weaponChangeRatePerLevel = weaponChangeRatePerLevel;
+            return this;
+        }
+
         public RAttributes buildWeapon() {
             RAttributes.swordProficiency = this.swordProficiency;
             RAttributes.axeProficiency = this.axeProficiency;
@@ -238,6 +385,15 @@ public record RAttributes() implements IAttributes {
             RAttributes.axeExperience = this.axeExperience;
             RAttributes.bowExperience = this.bowExperience;
             RAttributes.polearmExperience = this.polearmExperience;
+            RAttributes.swordExperienceRequired = this.swordExperienceRequired;
+            RAttributes.axeExperienceRequired = this.axeExperienceRequired;
+            RAttributes.bowExperienceRequired = this.bowExperienceRequired;
+            RAttributes.polearmExperienceRequired = this.polearmExperienceRequired;
+            RAttributes.swordGrowthRate = this.swordGrowthRate;
+            RAttributes.axeGrowthRate = this.axeGrowthRate;
+            RAttributes.bowGrowthRate = this.bowGrowthRate;
+            RAttributes.polearmGrowthRate = this.polearmGrowthRate;
+            RAttributes.weaponChangeRatePerLevel = this.weaponChangeRatePerLevel;
             return new RAttributes();
         }
 
@@ -248,6 +404,16 @@ public record RAttributes() implements IAttributes {
         private byte gambesonProficiency = 0;
         private byte chainmailProficiency = 0;
         private byte plateProficiency = 0;
+
+        private short gambesonExperienceRequired = 100;
+        private short chainmailExperienceRequired = 100;
+        private short plateExperienceRequired = 100;
+
+        private short gambesonGrowthRate = 120; // percentage change to needed experience per level
+        private short chainmailGrowthRate = 120; // percentage change to needed experience per level
+        private short plateGrowthRate = 120; // percentage change to needed experience per level
+
+        private byte armorChangeRatePerLevel = 5; // percentage change to growth rate per level, min 1 max 10
 
         private short gambesonExperience = 0;
         private short chainmailExperience = 0;
@@ -283,6 +449,41 @@ public record RAttributes() implements IAttributes {
             return this;
         }
 
+        public ArmorProficiencyBuilder setGambesonExperienceRequired(short gambesonExperienceRequired) {
+            this.gambesonExperienceRequired = gambesonExperienceRequired;
+            return this;
+        }
+
+        public ArmorProficiencyBuilder setChainmailExperienceRequired(short chainmailExperienceRequired) {
+            this.chainmailExperienceRequired = chainmailExperienceRequired;
+            return this;
+        }
+
+        public ArmorProficiencyBuilder setPlateExperienceRequired(short plateExperienceRequired) {
+            this.plateExperienceRequired = plateExperienceRequired;
+            return this;
+        }
+
+        public ArmorProficiencyBuilder setGambesonGrowthRate(short gambesonGrowthRate) {
+            this.gambesonGrowthRate = gambesonGrowthRate;
+            return this;
+        }
+
+        public ArmorProficiencyBuilder setChainmailGrowthRate(short chainmailGrowthRate) {
+            this.chainmailGrowthRate = chainmailGrowthRate;
+            return this;
+        }
+
+        public ArmorProficiencyBuilder setPlateGrowthRate(short plateGrowthRate) {
+            this.plateGrowthRate = plateGrowthRate;
+            return this;
+        }
+
+        public ArmorProficiencyBuilder setArmorChangeRatePerLevel(byte armorChangeRatePerLevel) {
+            this.armorChangeRatePerLevel = armorChangeRatePerLevel;
+            return this;
+        }
+
         public RAttributes buildArmor() {
             RAttributes.gambesonProficiency = this.gambesonProficiency;
             RAttributes.chainmailProficiency = this.chainmailProficiency;
@@ -290,6 +491,13 @@ public record RAttributes() implements IAttributes {
             RAttributes.gambesonExperience = this.gambesonExperience;
             RAttributes.chainmailExperience = this.chainmailExperience;
             RAttributes.plateExperience = this.plateExperience;
+            RAttributes.gambesonExperienceRequired = this.gambesonExperienceRequired;
+            RAttributes.chainmailExperienceRequired = this.chainmailExperienceRequired;
+            RAttributes.plateExperienceRequired = this.plateExperienceRequired;
+            RAttributes.gambesonGrowthRate = this.gambesonGrowthRate;
+            RAttributes.chainmailGrowthRate = this.chainmailGrowthRate;
+            RAttributes.plateGrowthRate = this.plateGrowthRate;
+            RAttributes.armorChangeRatePerLevel = this.armorChangeRatePerLevel;
             return new RAttributes();
         }
 
@@ -297,9 +505,26 @@ public record RAttributes() implements IAttributes {
 
     public static class SkillProficiencyBuilder { // start skill proficiency builder
 
-        private byte tacticalProficiency = 0;
-        private byte survivalProficiency = 0;
-        private byte medicalProficiency = 0;
+        private byte tacticalProficiency = 1;
+        private byte survivalProficiency = 1;
+        private byte medicalProficiency = 1;
+
+        private byte baseTacticalExperienceRequired = 100;
+        private byte baseSurvivalExperienceRequired = 100;
+        private byte baseMedicalExperienceRequired = 100;
+
+        private final double skillChangeRatePerLevel = 1.05; // percentage change to growth rate per level, min 1 max 10
+        private final double baseSkillGrowthRate = 1.20; // percentage change to needed experience per level
+        private final double totalGrowthRate = skillChangeRatePerLevel * baseSkillGrowthRate;
+
+
+        private double tacticalGrowthRate = totalGrowthRate * tacticalProficiency; // percentage change to needed experience per level
+        private double survivalGrowthRate = totalGrowthRate * survivalProficiency; // percentage change to needed experience per level
+        private double medicalGrowthRate =  totalGrowthRate * medicalProficiency; // percentage change to needed experience per level
+
+        private short tacticalExperienceRequired = 100;
+        private short survivalExperienceRequired = 100;
+        private short medicalExperienceRequired = (short) (baseMedicalExperienceRequired * medicalGrowthRate * medicalProficiency);
 
         private short tacticalExperience = 0;
         private short survivalExperience = 0;
@@ -335,6 +560,58 @@ public record RAttributes() implements IAttributes {
             return this;
         }
 
+        public SkillProficiencyBuilder setBaseTacticalExperienceRequired(byte baseTacticalExperienceRequired) {
+            this.baseTacticalExperienceRequired = baseTacticalExperienceRequired;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setBaseSurvivalExperienceRequired(byte baseSurvivalExperienceRequired) {
+            this.baseSurvivalExperienceRequired = baseSurvivalExperienceRequired;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setBaseMedicalExperienceRequired(byte baseMedicalExperienceRequired) {
+            this.baseMedicalExperienceRequired = baseMedicalExperienceRequired;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setTacticalExperienceRequired(short tacticalExperienceRequired) {
+            this.tacticalExperienceRequired = tacticalExperienceRequired;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setSurvivalExperienceRequired(short survivalExperienceRequired) {
+            this.survivalExperienceRequired = survivalExperienceRequired;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setMedicalExperienceRequired(short medicalExperienceRequired) {
+            this.medicalExperienceRequired = medicalExperienceRequired;
+            return this;
+        }
+
+
+
+        public SkillProficiencyBuilder setTacticalGrowthRate(double tacticalGrowthRate) {
+            this.tacticalGrowthRate = tacticalGrowthRate;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setSurvivalGrowthRate(double survivalGrowthRate) {
+            this.survivalGrowthRate = survivalGrowthRate;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setMedicalGrowthRate(double medicalGrowthRate) {
+            this.medicalGrowthRate = medicalGrowthRate;
+            return this;
+        }
+
+        public SkillProficiencyBuilder setSkillChangeRatePerLevel(double skillChangeRatePerLevel) {
+            this.skillChangeRatePerLevel = skillChangeRatePerLevel;
+            return this;
+        }
+
         public RAttributes buildSkill() {
             RAttributes.tacticalProficiency = this.tacticalProficiency;
             RAttributes.survivalProficiency = this.survivalProficiency;
@@ -342,6 +619,16 @@ public record RAttributes() implements IAttributes {
             RAttributes.tacticalExperience = this.tacticalExperience;
             RAttributes.survivalExperience = this.survivalExperience;
             RAttributes.medicalExperience = this.medicalExperience;
+            RAttributes.baseTacticalExperienceRequired = this.baseTacticalExperienceRequired;
+            RAttributes.baseSurvivalExperienceRequired = this.baseSurvivalExperienceRequired;
+            RAttributes.baseMedicalExperienceRequired = this.baseMedicalExperienceRequired;
+            RAttributes.tacticalExperienceRequired = this.tacticalExperienceRequired;
+            RAttributes.survivalExperienceRequired = this.survivalExperienceRequired;
+            RAttributes.medicalExperienceRequired = this.medicalExperienceRequired;
+            RAttributes.tacticalGrowthRate = this.tacticalGrowthRate;
+            RAttributes.survivalGrowthRate = this.survivalGrowthRate;
+            RAttributes.medicalGrowthRate = this.medicalGrowthRate;
+            RAttributes.skillChangeRatePerLevel = this.skillChangeRatePerLevel;
             return new RAttributes();
         }
 
