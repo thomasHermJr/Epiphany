f4b921d04882f939d0341a7e0d0831ff37862a2b
Thomas Herm Jr <tjherm01@louisville.edu>
2025-10-06 05:46:42 -0400

    Accept update
    Will be reformatting attributes; changes listed below.
    
    * Minor changes and notes added
    
    * Minor commit - added to TODO in RAttributes.java
    
    * Created Depreciated package to keep reference to older code. Moved RAttributes.java and BaseAttributes.java to Depreciated package. Overrode depreciated files to file type txt.
    
    * Added depreciation notes, added README.md to Depreciated package, added startingValue variable for EAttributesMain.java

---
+++ src/main/java/org/epiphany/Depreciated/README.md
@@ -0,0 +1,4 @@
+# Depreciated Code
+## Items in this folder are deprecated and no longer maintained. They are kept here for reference purposes only.
+
+## Depreciated files are changed to txts to avoid confusion and wrongful compilation.
--- src/main/java/org/epiphany/Enums/Systems/Character/Attributes/EAttributesMain.java
+++ src/main/java/org/epiphany/Enums/Systems/Character/Attributes/EAttributesMain.java
@@ -5,77 +5,102 @@ public enum EAttributesMain {
     STRENGTH("Strength",
             "Measures physical power; bonus to physical attack, heavy weapon proficiency gain, and chainmail proficiency gain.",
             1,
-            1,
-            20,
+            5,
+            50,
             1.5,
             1.25),
 
     DEXTERITY("Dexterity",
             "Measures agility and reflexes; bonus to ranged attack, light weapon proficiency gain, and gambeson proficiency gain.",
             1,
-            1,
-            20,
+            5,
+            50,
             1.5,
             1.25),
 
     INTELLIGENCE("Intelligence",
             "Measures reasoning and memory; bonus to skill proficiency gain.",
             1,
-            1,
-            20,
+            5,
+            50,
             1.5,
             1.25),
 
     WISDOM("Wisdom",
             "Measures perception and insight; bonus to life proficiency gain.",
             1,
-            1,
-            20,
+            5,
+            50,
             1.5,
             1.25),
 
     CONSTITUTION("Constitution",
             "Measures endurance and health; bonus to max health, stamina, and plate proficiency gain.",
             1,
-            1,
-            20,
+            5,
+            50,
             1.5,
             1.25),
 
     CHARISMA("Charisma",
             "Measures force of personality; bonus to personal proficiency gain.",
             1,
-            1,
-            20,
+            5,
+            50,
             1.5,
             1.25);
 
 
-    private String attributeName;
-    private String description;
-    private int minValue;
-    private int currentValue;
-    private int maxValue;
+    private final String attributeName;
+    private final String description;
+    private final int minValue;
+    private final int startingValue;
+    private final int maxValue;
     private double derivedBonus; // bonus to derived attributes
     private double proficiencyBonus; // bonus to proficiency gain
 
-    EAttributesMain(String attributeName, String description, int minValue, int currentValue, int maxValue, double derivedBonus, double proficiencyBonus) {
+    /* getters */
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public int getStartingValue() {
+        return startingValue; // default starting value for all attributes
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public double getDerivedBonus() {
+        return derivedBonus;
+    }
+
+    public double getProficiencyBonus() {
+        return proficiencyBonus;
+    }
+
+    EAttributesMain(String attributeName, String description, int minValue, int startingValue, int maxValue, double derivedBonus, double proficiencyBonus) {
+
         this.attributeName = attributeName;
         this.description = description;
         this.minValue = minValue;
-        this.currentValue = currentValue;
+        this.startingValue = startingValue;
         this.maxValue = maxValue;
         this.derivedBonus = derivedBonus;
         this.proficiencyBonus = proficiencyBonus;
-        if (currentValue <= 1) {
-            derivedBonus = 1;
-            proficiencyBonus = 1;
-        } else {
-            derivedBonus = currentValue * 1.5;
-            proficiencyBonus = currentValue * 1.25;
-        }
-            this.derivedBonus = derivedBonus;
-            this.proficiencyBonus = proficiencyBonus;
+        this.derivedBonus = derivedBonus;
+        this.proficiencyBonus = proficiencyBonus;
+
     } // end constructor
 
 } // end enum
